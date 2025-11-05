a4e04c1566ad347d11946bf786ebcb1c66954a59
Laura Calderón <laurajimenaflorez590@gmail.com>
2025-11-05 10:21:50 -0500

    feat: Implementar funcionalidad de calificaciones y navegación

---
+++ src/components/EssentialLink.vue
@@ -0,0 +1,60 @@
+<template>
+  <q-item
+    clickable
+    tag="a"
+    :to="link"
+    active-class="my-menu-link"
+  >
+    <q-item-section
+      v-if="icon"
+      avatar
+    >
+      <q-icon :name="icon" />
+    </q-item-section>
+
+    <q-item-section>
+      <q-item-label>{{ title }}</q-item-label>
+    </q-item-section>
+  </q-item>
+</template>
+
+<style lang="scss">
+.my-menu-link {
+  background-color: transparent !important;
+  position: relative;
+}
+
+.my-menu-link::after {
+  content: '';
+  position: absolute;
+  bottom: 0;
+  left: 16px;
+  right: 16px;
+  height: 3px;
+  background-color: #3B82F6; // Un azul brillante para la selección
+}
+</style>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'EssentialLink',
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+
+    link: {
+      type: String,
+      default: '#'
+    },
+
+    icon: {
+      type: String,
+      default: ''
+    }
+  }
+})
+</script>
