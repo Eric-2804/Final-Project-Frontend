bbd73a69153d032172cadce156de5fab79494152
Eric-2804 <ericdayan13@gmail.com>
2025-11-06 14:09:02 -0500

    Agregado token de acceso en archivo .env.example

---
+++ src/services/api.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "https://proyecto-final-ptwh.onrender.com", // tu backend
+  headers: {
+    "Content-Type": "application/json"
+  }
+});
+
+export default api;
