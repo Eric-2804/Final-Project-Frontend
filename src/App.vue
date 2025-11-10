b053156387ea63f9cccb51fa8bf4ce7739f6d34e
Shanty1239 <danielsilvaagredo22@gmail.com>
2025-11-10 16:52:03 -0500

    correcciones de routes y main/(pinia)

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,7 @@
-<script setup>
-import login from './views/login.vue';
-import router from './router/index.js'
-</script>
-
 <template>
   <div>
     <q-btn flat label="login" to="/" />
-  
-    
+
     <router-view />
   </div>
 </template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,9 @@
-// por favor coordinar y verificar las rutas con el equipo de desarrollo antes de hacer cambios
 import { createRouter, createWebHistory } from 'vue-router'
 import login from '../views/login.vue'
 
+
 const routes = [
-  {
-    path: '/',
-    name: 'login',
-    component: login
-  }
+  { path: '/', name: 'login', component: login },
 ]
 
 const router = createRouter({
