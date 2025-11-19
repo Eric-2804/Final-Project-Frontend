fce734847f55b9a6c0653097a51a6b878fb19334
QuesitoSucio <mantillatorresduvanandres@gmail.com>
2025-11-19 00:54:06 -0500

    academicLoad

--- src/router/index.js
+++ src/router/index.js
@@ -17,6 +17,7 @@ import EnrollmentsView from '../views/guardianViews/EnrollmentsView.vue'
 import GradesView from '../views/guardianViews/GradesView.vue'
 import TeacherView from '../views/guardianViews/TeacherView.vue'
 import StudentInfoView from '../views/guardianViews/StudentInfoView.vue'
+import AcademicLoad from '../views/guardianViews/AcademicLoad.vue'
 
 // vistas del coordinador
 import dashboard from '../views/coordinatorViews/dashboardView.vue'
@@ -55,6 +56,7 @@ const routes = [
   { path: '/GradesView', name: 'GradesView', component: GradesView},
   { path: '/TeacherView', name: 'TeacherView', component: TeacherView},
   { path: '/StudentInfoView', name: 'StudentInfoView', component: StudentInfoView},
+  { path: '/AcademicLoad', name: 'AcademicLoad', component: AcademicLoad},
 
 // rutas del coordinador
   { path: '/dashboardCoordinator', name: 'DashboardCoordinador', component: dashboard },
