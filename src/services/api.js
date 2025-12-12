fa7d76316eecfdbfd141fd5bd25fb60a88aa9280
Shanty1239 <danielsilvaagredo22@gmail.com>
2025-12-11 23:38:02 -0500

    add

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://proyecto-final-ptwh.onrender.com/',
+  baseURL: 'https://proyecto-final-ptwh.onrender.com',
   timeout: 70000,
 });
 
