c06f6e14fa649ce9433cb28d676f2920b1fabf5e
Shanty1239 <danielsilvaagredo22@gmail.com>
2025-12-12 11:20:05 -0500

    actualizar

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -56,7 +56,7 @@ const linksByRole = {
     { title: 'Sedes', icon: 'business', link: '/secretaria/headquarters' },
     { title: 'Matrículas', icon: 'app_registration', link: '/secretaria/registrations' },
     { title: 'Gestión Académica', icon: 'work', link: '/secretaria/academicManagement' },
-    { title: 'Profesores', icon: 'school', link: '/secretary/teacher' },
+    { title: 'Profesores', icon: 'school', link: '/secretaria/teacher' },
     { title: 'Grupos', icon: 'group', link: '/secretary/group' },
     { title: 'Áreas y Materias', icon: 'class', link: '/management/materia-area' },
     { title: 'Periodos', icon: 'event', link: '/secretaria/periodo' },
--- src/router/index.js
+++ src/router/index.js
@@ -35,7 +35,7 @@ const routes = [
     component: MainLayout,
     children: [
       
-      { path: 'secretary/teacher', name : 'Gestion de profesores', component: teacher},
+      { path: 'secretaria/teacher', name : 'Gestion de profesores', component: teacher },
       { path: 'secretaria/dashboard', name: 'SecretariaDashboard', component: DashboardSecretaria },
       { path: 'secretaria/headquarters', name: 'sedes_Secretaria', component: SedesSecretaria },
       { path: 'secretaria/academicManagement', name: "Gestion_Academica", component: AcademicManagementView },
