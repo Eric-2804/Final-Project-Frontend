3a0ead1ce29e3024963431774e0e5a16cd6b0068
Shanty1239 <danielsilvaagredo22@gmail.com>
2025-12-11 23:13:12 -0500

    add

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   baseURL: 'https://proyecto-final-ptwh.onrender.com/',
-  timeout: 100000,
+  timeout: 30000,
 });
 
 api.interceptors.request.use((config) => {
